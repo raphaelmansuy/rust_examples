7e981bcbab116fa61882e66f201f4bcdbd636dd5
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-03-29 19:07:44 +0100

    update

--- jsonl-example/client/src/main.rs
+++ jsonl-example/client/src/main.rs
@@ -1,8 +1,8 @@
-use futures::StreamExt;
+use futures::stream::{self, StreamExt};
+use serde::{Deserialize};
+use std::io::{BufRead, BufReader, Cursor};
 use reqwest::Client;
-use serde::Deserialize;
 use std::error::Error;
-use futures::stream::{self, BoxStream};
 
 #[derive(Deserialize, Debug)]
 struct User {
@@ -10,42 +10,44 @@ struct User {
     name: String,
 }
 
-async fn fetch_users(client: &Client, url: &str) -> Result<BoxStream<'static, Result<User, Box<dyn Error>>>, Box<dyn Error>> {
-    let response = client.get(url).send().await?.bytes_stream();
-
-    let user_stream = stream::unfold(response, |mut response| async {
-        match response.next().await {
-            Some(Ok(chunk)) => {
-                let json_str = match std::str::from_utf8(&chunk) {
-                    Ok(s) => s,
-                    Err(e) => return Some((Err(Box::new(e) as Box<dyn Error>), response)),
-                };
-                match serde_json::from_str::<User>(json_str) {
-                    Ok(user) => Some((Ok(user), response)),
-                    Err(e) => Some((Err(Box::new(e) as Box<dyn Error>), response)),
-                }
-            },
-            Some(Err(e)) => Some((Err(Box::new(e) as Box<dyn Error>), response)),
-            None => None, // End of stream
+fn create_line_stream<R: BufRead>(reader: R) -> impl Iterator<Item = Result<String, std::io::Error>> {
+    let mut buf_reader = BufReader::new(reader);
+    std::iter::from_fn(move || {
+        let mut line = String::new();
+        match buf_reader.read_line(&mut line) {
+            Ok(0) => None,
+            Ok(_) => Some(Ok(line.trim_end().to_string())),
+            Err(e) => Some(Err(e)),
         }
-    }).boxed();
+    })
+}
 
-    Ok(user_stream)
+async fn fetch_lines(url: &str) -> Result<impl Iterator<Item = Result<String, Box<dyn Error + Send + Sync>>>, Box<dyn Error + Send + Sync>> {
+    let client = Client::new();
+    let response = client.get(url).send().await?;
+
+    let status = response.status();
+    let body = response.text().await?;
+    let reader = Cursor::new(body);
+
+    if status.is_success() { // Use the stored status
+        Ok(create_line_stream(reader).map(|r| r.map_err(|e| e.into())))
+    } else {
+        let error_msg = format!("Error: {}", status);
+        Err(std::io::Error::new(std::io::ErrorKind::Other, error_msg).into())
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let client = Client::new();
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let url = "http://127.0.0.1:8080/users";
+    let mut lines = fetch_lines(url).await?;
+    let users: Vec<User> = lines
+        .by_ref()
+        .filter_map(|line| line.ok())
+        .filter_map(|line| serde_json::from_str::<User>(&line).ok())
+        .collect();
 
-    let user_stream = fetch_users(&client, url).await?;
-
-    user_stream.for_each(|user_result| async {
-        match user_result {
-            Ok(user) => println!("User: {:?}", user),
-            Err(e) => eprintln!("Error: {}", e),
-        }
-    }).await;
-
+    println!("{:#?}", users);
     Ok(())
 }
